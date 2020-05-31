c9470e04935b75bfdfb2f23a35dbaf71606038cc
Tom Jobbins <tomjobbins@gmail.com>
2020-05-31 12:35:16 +0100

    Remove unnecessary extra folder in WolvenKit.WpfControlLibrary.  Creation of basic outline of CR2WImportExport WPF UserControl.

---
+++ WolvenKit/Forms/CR2WImportExportWPF.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace WolvenKit.Forms
+{
+    partial class CR2WImportExportWPF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CR2WImportExportWPF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "CR2WImportExportWPF";
+            this.Text = "CR2W Import & Export";
+            this.Load += new System.EventHandler(this.CR2WImportExportWPF_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ WolvenKit/Forms/CR2WImportExportWPF.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Forms.Integration;
+using WeifenLuo.WinFormsUI.Docking;
+using WolvenKit.App.ViewModels;
+using WolvenKit.WpfControlLibrary.CR2WImportExport;
+
+namespace WolvenKit.Forms
+{
+    public partial class CR2WImportExportWPF : DockContent
+    {
+        private ElementHost ctrlHost;
+        private CR2WImportExportControl wpfcontrol;
+        private IViewModel viewModel;
+
+        public CR2WImportExportWPF()
+        {
+            InitializeComponent();
+        }
+
+        private void CR2WImportExportWPF_Load(object sender, EventArgs e)
+        {
+            // WPF ELementhosting
+            ctrlHost = new ElementHost
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(ctrlHost);
+            wpfcontrol = new CR2WImportExportControl(viewModel);
+            wpfcontrol.InitializeComponent();
+            ctrlHost.Child = wpfcontrol;
+            //wpfcontrol.Loaded += new RoutedEventHandler(wpfCtrl_Loaded);
+        }
+    }
+}
